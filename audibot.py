5a8bb91d658fab923dee26fb4fd68224e6f423cd
VerstandTsai <hackertsaigenz@gmail.com>
2022-03-26 18:53:25 +0800

    Initial commit

---
+++ audibot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+import subprocess
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'The bot has logged in as {client.user}.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!getaudio'):
+        await message.channel.send('正在下載音訊...')
+        url = message.content.split()[1]
+        subprocess.run(f'yt-dlp --extract-audio --audio-format mp3 -o audio.mp3 {url}'.split())
+        await message.channel.send(file=discord.File('audio.mp3'))
+        subprocess.run('rm audio.mp3'.split())
+
+client.run(os.getenv('TOKEN'))
