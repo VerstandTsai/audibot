84f25054eede7e0a1ce792fdec96cffdc44c74be
VerstandTsai <hackertsaigenz@gmail.com>
2022-04-05 00:10:00 +0800

    Added a feature to play music from url in voice channels

--- audibot.py
+++ audibot.py
@@ -25,7 +25,7 @@ async def help(ctx, botname):
             '在語音頻道中播放音樂：\n'
             f'!join               讓{bot.user.name}加入您所在的語音頻道\n'
             f'!leave              讓{bot.user.name}離開語音頻道\n'
-            '!play [編號/網址]   播放清單中的音樂\n'
+            '!play [編號/網址]   從清單中的第一項開始播放音樂\n'
             '                    若有指定編號，則播放清單中該編號的音樂\n'
             '                    若有指定網址，則播放該網址中的音樂\n'
             '!pause              暫停目前播放的音樂\n'
@@ -50,7 +50,7 @@ async def getaudio(ctx, url):
     }
     with YoutubeDL(ydl_opts) as ydl:
         title = ydl.extract_info(url, download=False)['title']
-        await ctx.send(f'正在下載{title}...')
+        await ctx.send(f'正在下載 {title}...')
         ydl.download([url])
     await ctx.send(file=discord.File('audio.mp3'))
     os.remove('audio.mp3')
@@ -67,13 +67,13 @@ async def getvideo(ctx, url):
             'outtmpl': f'./downloads/{info["id"]}.mp4'
     }
     with YoutubeDL(ydl_opts) as ydl:
-        await ctx.send(f'正在下載{info["title"]}...')
+        await ctx.send(f'正在下載 {info["title"]}...')
         ydl.download([url])
     await ctx.send(
             '下載完成，點擊以下連結以下載\n'
             f'https://audibot-discord.herokuapp.com/downloads/{info["id"]}'
     )
-'''
+
 @bot.command()
 async def join(ctx):
     if not ctx.author.voice:
@@ -82,7 +82,7 @@ async def join(ctx):
     await ctx.author.voice.channel.connect()
     os.mkdir(f'./playlists/{ctx.guild.id}')
     with open(f'./playlists/{ctx.guild.id}/playlist.json', 'w') as fp:
-        pass
+        json.dump([], fp)
 
 @bot.command()
 async def leave(ctx):
@@ -93,7 +93,26 @@ async def leave(ctx):
 
 @bot.command()
 async def play(ctx, arg=None):
-    pass
+    vc = ctx.voice_client
+    if arg == None:
+        pass
+    elif arg.isdigit():
+        pass
+    else:
+        ydl_opts = {
+                'format': 'bestaudio',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3'
+                }],
+                'outtmpl': 'audio.mp3'
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            title = ydl.extract_info(url, download=False)['title']
+            await ctx.send(f'正在獲取 {title}...')
+            ydl.download([url])
+        vc.play(discord.FFmpegPCMAudio('audio.mp3'))
+        os.remove('audio.mp3')
 
 @bot.command()
 async def pause(ctx):
@@ -129,10 +148,35 @@ async def list(ctx):
 
 @bot.command()
 async def add(ctx, url):
-    pass
+    info = YoutubeDL({}).extract_info(url, download=False)
+    ydl_opts = {
+            'format': 'bestaudio',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3'
+            }],
+            'outtmpl': f'./playlists/{ctx.guild.id}/{info["id"]}.mp3'
+    }
+    await ctx.send(f'正在加入 {info["title"]}...')
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    playlist = []
+    with open(f'./playlists/{ctx.guild.id}/playlist.json', 'r') as fp:
+        playlist = json.load(fp)
+    song = {}
+    song['title'] = info['title']
+    song['file'] = f'{info["id"]}.mp3'
+    playlist.append(song)
+    with open(f'./playlists/{ctx.guild.id}/playlist.json', 'w') as fp:
+        json.dump(playlist, fp)
 
 @bot.command()
 async def pop(ctx, num):
-    pass
-'''
+    playlist = []
+    with open(f'./playlists/{ctx.guild.id}/playlist.json', 'r') as fp:
+        playlist = json.load(fp)
+    playlist.pop(int(num)-1)
+    with open(f'./playlists/{ctx.guild.id}/playlist.json', 'w') as fp:
+        json.dump(playlist, fp)
+
 bot.run(os.getenv('TOKEN'))
